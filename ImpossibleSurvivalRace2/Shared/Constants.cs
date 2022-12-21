fb95af4c81c32115e613c86425eb02ad49d4987d
sippanas <alavicius.lukas@gmail.com>
2022-12-22 00:57:49 +0200

    Map creation refactoring

--- ImpossibleSurvivalRace2/Shared/Constants.cs
+++ ImpossibleSurvivalRace2/Shared/Constants.cs
@@ -3,5 +3,7 @@
     public static class Constants
     {
         public const int PLAYER_SIZE = 5;
+        public const int CANVAS_WIDTH = 1400;
+        public const int CANVAS_HEIGHT = 500;
     }
 }
