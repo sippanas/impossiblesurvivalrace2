32e79ed56f73ab6a4d64b43f63139f2290921772
Griske24 <justas.griskevicius@gmail.com>
2022-12-23 15:19:13 +0200

    Add Template Method design pattern

---
+++ ImpossibleSurvivalRace2/TemplateMethodDesignPattern/Start.cs
@@ -0,0 +1,37 @@
+﻿using Blazor.Extensions.Canvas.Canvas2D;
+using ImpossibleSurvivalRace2.Server;
+using ImpossibleSurvivalRace2.Shared.Models;
+using Microsoft.AspNetCore.SignalR.Client;
+using ImpossibleSurvivalRace2.Pages;
+
+namespace ImpossibleSurvivalRace2.TemplateMethodDesignPattern
+{
+    public class Start : StartAbstract
+    {
+        public bool HasStarted { get; set; }
+        public Start()
+        {
+            this.HasStarted = false;
+        }
+        public Start(bool hasStarted)
+        {
+            this.HasStarted = hasStarted;
+        }
+
+        public override void TemplateMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DrawMap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task SpawnPlayer(Player player, Canvas2DContext context, int x, int y, bool isLocalPlayer)
+        {
+            Pages.Lobby lobby = new();
+            await lobby.DrawPlayer(player, context, x, y, isLocalPlayer);
+        }
+    }
+}
---
+++ ImpossibleSurvivalRace2/TemplateMethodDesignPattern/StartAbstract.cs
@@ -0,0 +1,13 @@
+﻿using Blazor.Extensions.Canvas.Canvas2D;
+using ImpossibleSurvivalRace2.Shared.Models;
+
+namespace ImpossibleSurvivalRace2.TemplateMethodDesignPattern
+{
+    public abstract class StartAbstract
+    {
+
+        public abstract void TemplateMethod();
+        public abstract void DrawMap();
+        public abstract Task SpawnPlayer(Player player, Canvas2DContext context, int x, int y, bool isLocalPlayer);
+    }
+}
